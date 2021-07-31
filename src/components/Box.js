689027cb13cb5774f5bf2309aa74de30c2a54b0b
Bine Cadez <bine.cadez@gmail.com>
2021-07-31 17:08:43 +0200

    added sidebar

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,8 @@
-import React from "react"
-import "../style/box.css"
+import React from "react";
+import "../style/box.css";
 
 export default class Box extends React.Component {
-    render() {
-    return (
-        <div className = "box">
-        </div>
-        )
-    }
+  render() {
+    return <div className="box"></div>;
+  }
 }
