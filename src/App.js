d7fba9ac4b8aede968e21a9c41cd17be998b51bf
Bine Cadez <bine.cadez@gmail.com>
2021-07-31 16:31:43 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header.js'
+import Box from './components/Box.js'
+import { render } from '@testing-library/react';
+
+function App() {
+ return (
+ <div>
+  <Header></Header>
+  <Box></Box>
+  </div>
+ );
+}
+
+export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../style/box.css"
+
+export default class Box extends React.Component {
+    render() {
+    return (
+        <div className = "box">
+        </div>
+        )
+    }
+}
